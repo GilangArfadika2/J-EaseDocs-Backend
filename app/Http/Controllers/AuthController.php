a41529e827324963c4e8eaa1a0dc4ce6170e1933
gilang arfadika <gilang.arfadika@ui.ac.id>
2024-03-14 02:18:29 +0700

    fix backend API not accepting fronend request 25

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,8 +68,8 @@ class AuthController extends Controller
                 $token,      // Token value
                 60,          // Cookie expiration time in minutes
                 '/',         // Path
-                '.vercel.app',
-                false,       // Secure (set to true if using HTTPS)
+                'https://j-easedocs-frontend.vercel.app/',
+                true,       // Secure (set to true if using HTTPS)
                 true       // HTTP-only flag
             );
         } catch (ValidationException $e) {
