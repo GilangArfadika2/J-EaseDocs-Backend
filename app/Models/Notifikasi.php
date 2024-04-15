54b96ca5bce94810f8cb9f4adfc5cc91d50c4b98
gilang arfadika <gilang.arfadika@ui.ac.id>
2024-04-15 21:16:18 +0700

    add notifikasi  model

---
+++ app/Models/Notifikasi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Notifikasi extends Model
+{
+    use  HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $table = 'inbox';
+    public $timestamps = false;
+    protected $fillable = [
+       'id',
+        'message',
+        'user_id',
+        'letter_id',
+        'is_read',
+        'decision',
+        
+
+    ];
+
+   
+}
